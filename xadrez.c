dae738c14a0b9ada93291453012b6c8d64775a4d
Gabriel S Alm <103471990+GabrielSAlm1@users.noreply.github.com>
2026-04-21 17:47:55 -0300

    Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -4,11 +4,9 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-#include <stdio.h>
-
 int main() {
 
-  int torre, bispo = 0, rainha = 0;
+  int torre, bispo = 0, rainha = 0, cavalo = 1;
 
   printf("Movimentação torre.\n");
   for (torre = 0; torre < 5; torre++){
@@ -31,6 +29,18 @@ int main() {
     printf("Cima, Direita\n");
     bispo++;
   } while (bispo < 5);
+  printf("----------\n");
+
+  printf("Movimentação Cavalo.\n");
+  while (cavalo--)
+  {
+    for (int i = 0; i < 2; i++)
+    {
+      printf("Baixo\n");
+    }
+    printf("Esquerda\n");
+    
+  }
   printf("----------\n");
   
 
